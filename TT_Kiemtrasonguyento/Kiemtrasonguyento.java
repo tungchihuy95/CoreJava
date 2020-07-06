e98739e3de80d03ab1dacbbe5337ba83be91e9c8
tungchihuy95 <tungjpvn95@gmail.com>
2020-07-06 11:33:53 +0700

    ja

--- TT_Kiemtrasonguyento/Kiemtrasonguyento.java
+++ TT_Kiemtrasonguyento/Kiemtrasonguyento.java
@@ -1,4 +1,28 @@
 package TT_Kiemtrasonguyento;
 
+import java.util.Scanner;
+
 public class Kiemtrasonguyento {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number");
+        int number = sc.nextInt();
+        if (number < 2)
+            System.out.println(number + "is not a Prime");
+        else {
+            int i = 2;
+            boolean check = true;
+            while (i <= Math.sqrt(number)) {
+                if (number % i == 0) {
+                    check = false;
+                    break;
+            }
+                i++;
+        }
+            if (check)
+                System.out.println(number + "is a Prime");
+            else
+                System.out.println(number + "is not a Prime");
+    }
+}
 }
